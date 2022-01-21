acf0b6d7e22f89007af518aeafa8b242d664510e
Vodrochele999 <52430260+Vodrochele999@users.noreply.github.com>
2022-01-21 13:04:18 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Comment these lines to hide errors
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+//This Change is made by Salamn
+//Assignment 01 by Salman
+//Changed by Salman_Assignment_01
+
+init();
